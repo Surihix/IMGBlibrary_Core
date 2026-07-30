ca366e6aeeae839205a03df9aa73381e7ef978a3
Surihix <43111452+Surihix@users.noreply.github.com>
2026-07-31 00:19:11 +0530

    added preliminary IMGB loading type classes

---
+++ IMGBlibrary_Core/Load/IMGBLoad.cs
@@ -0,0 +1,78 @@
+﻿using IMGBlibrary_Core.Support;
+
+namespace IMGBlibrary_Core.Load
+{
+    /// <summary>
+    /// Provides a method for loading image files from the FINAL FANTASY XIII trilogy.
+    /// </summary>
+    public class IMGBLoad
+    {
+        /// <summary>
+        /// Use for unpacking image files.
+        /// </summary>
+        /// <param name="imgHeaderBlockFile">Header Block file path. should have the GTEX chunk.</param>
+        /// <param name="imgbFile">IMGB file path. the file has to be present.</param>
+        /// <param name="imgbPlatform">Platform of the header block file.</param>
+        /// <param name="showLog">Determine whether to show more messages related to this method's process.</param>
+        public static byte[] LoadIMGB(string imgHeaderBlockFile, string imgbFile, IMGBFlags.Platforms imgbPlatform, bool showLog)
+        {
+            var ddsData = Array.Empty<byte>();
+
+            var gtex = SharedMethods.GetGTEXInfo(imgHeaderBlockFile);
+
+            if (!gtex.IsValid)
+            {
+                SharedMethods.DisplayLogMessage("Unable to find GTEX chunk. skipped image loading.", true);
+                return ddsData;
+            }
+
+            if (imgbPlatform == IMGBFlags.Platforms.x360)
+            {
+                SharedMethods.DisplayLogMessage("Platform set to x360. loaded image file(s) will not be unswizzled.", true);
+            }
+
+            SharedMethods.DisplayLogMessage($"Image Format Value: {gtex.Format}", showLog);
+            SharedMethods.DisplayLogMessage($"Image MipCount: {gtex.MipCount}", showLog);
+            SharedMethods.DisplayLogMessage($"Image Type Value: {gtex.Type}", showLog);
+            SharedMethods.DisplayLogMessage($"Image Width: {gtex.Width}", showLog);
+            SharedMethods.DisplayLogMessage($"Image Height: {gtex.Height}", showLog);
+
+            if (!SharedMethods.CheckGTEXFormatAndType(gtex))
+            {
+                SharedMethods.DisplayLogMessage("Detected unknown format or type. skipped image loading.", true);
+                return ddsData;
+            }
+
+            // Open the IMGB file and start loading
+            // the images according to the image type
+            using (var imgbStream = new FileStream(imgbFile, FileMode.Open, FileAccess.ReadWrite))
+            {
+                IMGBLoadTypes.Platform = imgbPlatform;
+
+                switch (gtex.Type)
+                {
+                    // Classic type
+                    // Type 4 is for console versions
+                    case 0:
+                    case 4:
+                        ddsData = IMGBLoadTypes.UnpackClassic(imgHeaderBlockFile, gtex, imgbStream);
+                        break;
+
+                    // Cubemap type 
+                    // Type 5 is for console versions
+                    case 1:
+                    case 5:
+                        ddsData = IMGBLoadTypes.UnpackCubemap(imgHeaderBlockFile, gtex, imgbStream);
+                        break;
+
+                    // Volumemap type
+                    case 2:
+                        ddsData = IMGBLoadTypes.UnpackVolumemap(imgHeaderBlockFile, gtex, imgbStream);
+                        break;
+                }
+            }
+
+            return ddsData;
+        }
+    }
+}
---
+++ IMGBlibrary_Core/Load/IMGBloadTypes.cs
@@ -0,0 +1,32 @@
+﻿using IMGBlibrary_Core.Support;
+
+namespace IMGBlibrary_Core.Load
+{
+    internal class IMGBLoadTypes
+    {
+        public static IMGBFlags.Platforms Platform { get; set; }
+
+        #region Classic type
+        public static byte[] UnpackClassic(string imgHeaderBlockFile, GTEX gtex, FileStream imgbStream)
+        {
+            return null;
+        }
+        #endregion
+
+
+        #region Cubemap type
+        public static byte[] UnpackCubemap(string imgHeaderBlockFile, GTEX gtex, FileStream imgbStream)
+        {
+            return null;
+        }
+        #endregion
+
+
+        #region Volumemap type
+        public static byte[] UnpackVolumemap(string imgHeaderBlockFile, GTEX gtex, FileStream imgbStream)
+        {
+            return null;
+        }
+        #endregion
+    }
+}
